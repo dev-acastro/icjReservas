b4fe9d2cd393e38aec6adc78799d71c8b136f27e
Arturo Castro <armicasdi@gmail.com>
2020-09-11 15:33:48 -0600

    Arreglado Thanks You Page

--- resources/views/ThankYouPage.blade.php
+++ resources/views/ThankYouPage.blade.php
@@ -6,7 +6,7 @@
 <div  style="text-align: center;
                                     display: flex;
                                     justify-content: center;
-                                    padding: 50px;" >
+                                    padding: 10px;" >
 
     <div style="background-color: white;
                                             margin: 20px auto;
@@ -14,7 +14,7 @@
                                             border-bottom: #1b4b72 1px solid;">
 
         <div style="background-color: #1b4b72; padding: 30px; margin-bottom: 50px">
-            <img src="https://i.ibb.co/r6S8spK/Sin-t-tulo-1.png" alt="Sin-t-tulo-1" height="80px" border="0">
+            <img src="https://i.ibb.co/r6S8spK/Sin-t-tulo-1.png" alt="Sin-t-tulo-1" width="100%" border="0">
         </div>
 
 
@@ -40,7 +40,7 @@
 
         </table>
         <p>No Olvides los siguientes lineamientos</p>
-        <img src="https://i.ibb.co/3MwBxbL/Whats-App-Image-2020-09-09-at-9-12-13-AM.jpg" alt="Protocolo" border="0">
+       <div style="text-align: center"> <img src="https://i.ibb.co/3MwBxbL/Whats-App-Image-2020-09-09-at-9-12-13-AM.jpg" alt="Protocolo" border="0" width="90%"></div>
 
     </div>
 </div>
