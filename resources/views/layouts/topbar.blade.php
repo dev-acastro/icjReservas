070a037bc0a9cc1583ed87909976ed9ac96ae5ee
Arturo Castro <armicasdi@gmail.com>
2020-09-12 20:28:58 -0600

    Email Perfeccionados

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -36,11 +36,11 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img src="{{asset('images/logo.png')}}" width="270">
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                {{--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button>--}}
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                {{--<div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
@@ -50,7 +50,7 @@
                         <!-- Busqueda -->
 
 
-                            {{--<form id="search" action="{{ route('search') }}" method="POST"  >
+                            --}}{{--<form id="search" action="{{ route('search') }}" method="POST"  >
                                 @csrf
                                 <div class="input-group">
                                     <input type="email" name="email" class="form-control" placeholder="Buscar Reserva" aria-label="" aria-describedby="basic-addon1">
@@ -58,10 +58,10 @@
                                         <button class="btn btn-primary" type="submit">Buscar</button>
                                     </div>
                                 </div>
-                            </form>--}}
+                            </form>--}}{{--
 
                     </ul>
-                </div>
+                </div>--}}
             </div>
         </nav>
 
