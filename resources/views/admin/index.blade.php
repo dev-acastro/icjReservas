ec19507a01a54965768b5c94d7a2f9e95afe5acb
Arturo Castro <armicasdi@gmail.com>
2020-10-06 12:15:02 -0600

    -8 horas

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -52,16 +52,16 @@
 
                                     <div class="col-12" style="padding-bottom: 10px; margin-bottom: 10px; border-bottom: #d7d3d3 1px solid;">
                                         <a style="color: black" href="{{route('attendees.show', $time->date)}}">
-                                            <div class="col-sm-5">
-                                                <p style="font-size: 14px">{{$dias[date('w', strtotime($time->date))]}} {{date('d', strtotime($time->date))}} {{date('h:i', strtotime($time->date))}}{{date('A', strtotime($time->date))}}</p>
-                                                <p><span><div style="width: 100%; height: 10px; background-color: #808080"><div style="background-color: blueviolet; width:{{((($time->seats - $time->availableseats)*$time->seats) / $time->availableseats)}}%; height: 10px;"></div></div></span></p>
+                                            <div class="col-sm-8">
+                                                <div class="col-sm-6 ">
+                                                    <p style="font-size: 14px">{{$dias[date('w', strtotime($time->date))]}} {{date('d', strtotime($time->date))}} {{date('h:i', strtotime($time->date))}}{{date('A', strtotime($time->date))}}</p>
+                                                    <p><span><div style="width: 100%; height: 10px; background-color: #808080"><div style="background-color: blueviolet; width:{{((($time->seats - $time->availableseats)*$time->seats) / $time->availableseats)}}%; height: 10px;"></div></div></span></p>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <p style="font-size: 30px; color: grey">{{round(((($time->seats - $time->availableseats)*100) / $time->availableseats))}}%</p>
+                                                </div>
                                             </div>
                                         </a>
-
-                                        <div class="col-sm-3">
-                                            <p style="font-size: 30px; color: grey">{{round(((($time->seats - $time->availableseats)*$time->seats) / $time->availableseats))}}%</p>
-                                        </div>
-
                                         <div class="col-sm-4">
 
                                             <p style="font-size: 13px; margin-bottom: 0px;">Disponibles: {{$time->seats}}</p>
